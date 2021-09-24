e946ea7565be20c2679ca47df4e3b71808e98f8d
ShiftyKitty <cathalpmcgahan@gmail.com>
2021-09-24 17:54:07 +0000

    added stripe elements

--- checkout/views.py
+++ checkout/views.py
@@ -12,6 +12,7 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51JdIFMCgA9z4ehGFIyTn1oZcs37BznsXFZWsO40mEuSIqpnFIEDtekXyFS6iKe6D6GUhrhsnH9pA6y0IZruZ2jul00rOSUQ54w',
     }
 
     return render(request, template, context)
